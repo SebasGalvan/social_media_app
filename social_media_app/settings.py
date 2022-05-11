7420ec125f4bd0290be92432350828b082e7a45d
Sebastián Galván <sebastiangalvan.ar@gmail.com>
2022-05-11 18:59:34 -0300

    [ADD] Configuracion de app, vistas y url

--- social_media_app/settings.py
+++ social_media_app/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'social_media_app.urls'
