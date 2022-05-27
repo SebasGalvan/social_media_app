9ea4f46c08259d4cafcc7e2ec23f9ac34959bacf
Sebastián Galván <sebastiangalvan.ar@gmail.com>
2022-05-27 01:13:08 -0300

    [ADD] Log out + middlewares

--- core/views.py
+++ core/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.models import User, auth
-
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 from .models import Profile
 
 
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
 
@@ -30,8 +30,8 @@ def signup(request):
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
 
-                #Log user in and redirect to settings page
-                #create a Profile ojbect for the new user
+                # Log user in and redirect to settings page
+                # create a Profile ojbect for the new user
 
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
@@ -42,14 +42,11 @@ def signup(request):
         else:
             messages.info(request, 'Password not matching')
             return redirect('signup')
-
-        return render(request, 'signin.html')
     else:
         return render(request, 'signup.html')
 
 
 def signin(request):
-
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -60,6 +57,17 @@ def signin(request):
             return redirect('/')
         else:
             messages.info(request, 'Credentials Invalid')
-            return render('signin.html')
+            return render(request, 'signin.html')
     else:
         return render(request, 'signin.html')
+
+
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
+
+
+@login_required(login_url='signin')
+def settings(request):
+    return render(request, 'settings.html')
