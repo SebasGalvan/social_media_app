a099f01f5aad352eec199263c70bb3c1c04bd65a
Sebastián Galván <sebastiangalvan.ar@gmail.com>
2022-06-14 15:45:34 -0300

    [FIX] Creacion de la clave primaria de los posts

--- core/models.py
+++ core/models.py
@@ -19,7 +19,7 @@ class Profile(models.Model):
 
 
 class Post(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4())
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     user = models.CharField(max_length=100)
     image = models.ImageField(upload_to='post_images')
     caption = models.TextField()
