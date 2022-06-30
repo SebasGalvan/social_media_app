e8e08b1646aeee1acd434299437a7d879752bde2
Sebastián Galván <sebastiangalvan.ar@gmail.com>
2022-06-29 23:49:15 -0300

    [ADD] Path/Ruta para seguir / follower

--- core/urls.py
+++ core/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('upload', views.upload, name='upload'),
     path('like-post', views.like_post, name='like-post'),
     path('profile/<str:pk>', views.profile, name='profile'),
-    path('profile/<str:pk>', views.profile, name='profile'),
+    path('follow', views.follow, name='follow'),
 
 ]
